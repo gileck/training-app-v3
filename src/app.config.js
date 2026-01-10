0bf9742748699d213b5b7c9447fc00ec500f9026
gileck <gileck@wix.com>
2026-01-10 14:23:07 +0200

    update ownerTelegramChatId app.config.js

--- src/app.config.js
+++ src/app.config.js
@@ -3,5 +3,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 export const appConfig = {
     appName: 'training-app-v3',
     cacheType: isProduction ? 's3' : 's3',
-    dbName: 'training_app_v3_db'
+    dbName: 'training_app_v3_db',
+    ownerTelegramChatId: '-5272525461'
 };
